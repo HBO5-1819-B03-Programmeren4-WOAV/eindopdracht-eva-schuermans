3667b51c0947c9bec95f82c2f737fb8b73deda51
eva.schuermans <argali207@hotmail.com>
2019-02-05 20:28:19 +0100

    Still working on registrations page, i hope i can fix it before the deadline. All variabels are filled well, except with the foreach loop I have troubles filling the participants array...

---
+++ B03.EE.SchuermansEva.MVC/wwwroot/js/registrations.js
@@ -0,0 +1,58 @@
+﻿var apiURL = 'https://localhost:44335/api/';
+
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Loading registrations...',
+        activities: null,
+        participants: null,  
+        isReadOnly: true,
+        isEdit: false
+    },
+    created: function () {
+        var self = this;
+        self.fetchActivities(); 
+    },
+    methods: {
+        fetchActivities: function () {
+            self = this;
+            fetch(`${apiURL}Activities/Basic`)
+                .then(res => res.json())
+                .then(function (activities) {
+                    activities.forEach(function (activity, i) {
+                        activity.isActive = false;
+                    });
+                    self.activities = activities;
+                    self.message = 'Overview';
+                    if (self.activities.length > 0) {
+                        self.fetchActivityParticipants(self.activities[0]);
+                    }
+                })
+                .catch(err => console.error('Fout: ' + err));
+        },
+        fetchActivityParticipants: function (activity) {
+            self = this;
+            if (!self.isReadOnly) return;
+            fetch(`${apiURL}Registrations`)
+                .then(res => res.json())
+                .then(function (registrations) { 
+                    registrations.forEach(function (registration, i) {
+                        if (registration.activityId == activity.id) {
+                            participants.push(registration.user); 
+                        }
+                    });
+                    self.makeActivityActive(activity.id); 
+                })
+                .catch(err => console.error('Fout: ' + err));
+        },  
+        makeActivityActive: function (activityid) {
+            self.activities.forEach(function (activity, i) {
+                activity.isActive = activity.id === activityid ? true : false;
+            });
+        },
+        getActivityClass: function (activity) {
+            if (activity.isActive) return 'list-group-item active';
+            return 'list-group-item';
+        },  
+    }
+});
